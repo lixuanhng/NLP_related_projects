b58c6f455840cfb7bbf8d895ae2b72fca4bcba84
lixuanhng <lixh2100@gmail.com>
2021-06-10 16:44:18 +0800

    update esim

--- ESIM/args.py
+++ ESIM/args.py
@@ -6,12 +6,14 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="train")
-parser.add_argument("--w2v_model_path", type=str, default="./word2vec_model/word2vec.model",help="word2vec_model")
+parser.add_argument("--w2v_model_path", type=str, default="./word2vec_model/word2vec.model",help="word2vec model")
+parser.add_argument("--train_path", type=str, default="./inputs/train.csv",help="train file")
+parser.add_argument("--vocab_file", type=str, default="./inputs/vocab.txt",help="vocab file")
+parser.add_argument("--max_len", type=str, default=20, help="max length")
 
-parser.add_argument("--train_path", type=str, default="./dataset_0202/train.txt",help="train file")  # 训练数据位置
+
 parser.add_argument("--test_path", type=str, default="./dataset_0202/test.txt",help="test file")  # 测试数据位置
 parser.add_argument("--output_dir", type=str, default="./checkpoints_0202_test1/",help="output_dir")  # 模型保存位置（待写入）
-parser.add_argument("--vocab_file", type=str, default="./dataset_0202/vocab.txt",help="vocab_file")  # 词表位置（待写入）
 parser.add_argument("--tag_file", type=str, default="./dataset_0202/tags.txt",help="tag_file")  #  标签位置（待写入）
 parser.add_argument("--batch_size", type=int, default=32,help="batch_size")
 parser.add_argument("--hidden_num", type=int, default=512,help="hidden_num")  # 神经网络层神经元个数（虽然传入了模型，但是目前没有使用）
@@ -19,5 +21,13 @@ parser.add_argument("--embedding_size", type=int, default=300,help="embedding_si
 parser.add_argument("--pretrain_embedding_vec", type=str, default='./pretrain_vec/token_vec_300.bin',help="pretrain_embedding_vec")  # 词向量文件
 parser.add_argument("--epoch", type=int, default=8,help="epoch")
 parser.add_argument("--lr", type=float, default=0.01,help="lr")
-parser.add_argument("--require_improvement", type=int, default=100,help="require_improvement")
+parser.add_argument("--require_improvement", type=int, default=100, help="require_improvement")
 args = parser.parse_args()
+
+
+import numpy as np
+
+x = (np.ones((2, 5)) * 0.).astype('int32')
+trunc = [1,1,1,0,0]
+x[0, :len(trunc)] = trunc
+print(x)
--- ESIM/utils.py
+++ ESIM/utils.py
@@ -5,27 +5,139 @@
 
 import pandas as pd
 import os
-import args
+from args import args
 import jieba
 import re
-from gensim.models import Word2Vec
+from gensim.models.word2vec import Word2Vec
 import numpy as np
+
+
+
+def shuffle(*arrs):
+    """
+    打乱数据
+    Arguments:
+        *arrs: 数组数据
+    Returns:
+        shuffle后的数据
+    """
+    arrs = list(arrs)
+    for i, arr in enumerate(arrs):
+        assert len(arrs[0]) == len(arrs[i])
+        # 转换为numpy数组
+        arrs[i] = np.array(arr)
+    # 将数据打乱
+    p = np.random.permutation(len(arrs[0]))
+    # 三个数据进行融合
+    return tuple(arr[p] for arr in arrs)
+
+
+def load_char_vocab():
+    """
+    加载字典，生成 {word: id} 和 {id: word}
+    :return:
+    """
+    # path = os.path.join(os.path.dirname(__file__), args.vocab_file)
+    vocab = [line.strip() for line in open(args.vocab_file, encoding='utf-8').readlines()]
+    word2idx = {word: index for index, word in enumerate(vocab)}
+    idx2word = {index: word for index, word in enumerate(vocab)}
+    return word2idx, idx2word
+
+
+def pad_sequences(sequences, maxlen=None, dtype='int32', padding='post',
+                  truncating='post', value=0.):
+    """ pad_sequences
+    把序列长度转变为一样长的，如果设置了maxlen则长度统一为maxlen，如果没有设置则默认取
+    最大的长度。填充和截取包括两种方法，post与pre，post指从尾部开始处理，pre指从头部
+    开始处理，默认都是从尾部开始。
+
+    Arguments:
+        sequences: 序列
+        maxlen: int 最大长度
+        dtype: 转变后的数据类型
+        padding: 填充方法'pre' or 'post'
+        truncating: 截取方法'pre' or 'post'
+        value: float 填充的值
+
+    Returns:
+        x: numpy array 填充后的序列维度为 (number_of_sequences, maxlen)
+    """
+    # 获取每个序列的长度
+    lengths = [len(s) for s in sequences]
+
+    nb_samples = len(sequences)  # 样本长度
+    if maxlen is None:
+        maxlen = np.max(lengths)
+
+    # 产生维度为 (nb_samples, maxlen) 填充矩阵，填充元素为 value
+    x = (np.ones((nb_samples, maxlen)) * value).astype(dtype)
+    for idx, s in enumerate(sequences):
+        if len(s) == 0:
+            continue  # empty list was found
+        if truncating == 'pre':
+            trunc = s[-maxlen:]
+        elif truncating == 'post':
+            trunc = s[:maxlen]
+        else:
+            # 返回【截取方法不明确】
+            raise ValueError("Truncating type '%s' not understood" % padding)
+
+        # 将得到的填充结果trunc替换为
+        if padding == 'post':
+            x[idx, :len(trunc)] = trunc
+        elif padding == 'pre':
+            x[idx, -len(trunc):] = trunc
+        else:
+            # 返回【填充方法不明确】
+            raise ValueError("Padding type '%s' not understood" % padding)
+    return x
+
+
+def char_index(p_sentences, h_sentences):
+    """
+    将字转化为index
+    :param p_sentences: primise 句子列表
+    :param h_sentences: hypothesis 句子列表
+    :return:
+    """
+    word2idx, idx2word = load_char_vocab()
+
+    p_list, h_list = [], []
+    # 每次取一个句子对，将句子中的所有字转化为id后，存入列表
+    for p_sentence, h_sentence in zip(p_sentences, h_sentences):
+        p = [word2idx[word.lower()] for word in p_sentence if len(word.strip()) > 0 and word.lower() in word2idx.keys()]
+        h = [word2idx[word.lower()] for word in h_sentence if len(word.strip()) > 0 and word.lower() in word2idx.keys()]
+
+        p_list.append(p)
+        h_list.append(h)
+    # 对文本长度进行统一
+    p_list = pad_sequences(p_list, maxlen=args.max_len)
+    h_list = pad_sequences(h_list, maxlen=args.max_len)
+
+    return p_list, h_list
 
 
 def load_all_data(path, data_size=None):
     # 载入训练好的词向量
-    model = Word2Vec.load(args.word2vec_model)
+    model = Word2Vec.load("./word2vec_model/word2vec.model")
     # 输入数据的格式为csv，sentence1,sentence2,label
-    # 参考代码中的 vocab.txt 比较好，应该利用起来
+    # 参考代码中的 vocab.txt 及所有文本数据都应该利用起来
+    df = pd.read_csv(path)
 
     # 分别从原始数据中获取 data_size 组数组
     p = df['sentence1'].values[0:data_size]  # premise
     h = df['sentence2'].values[0:data_size]  # hypothesis
     label = df['label'].values[0:data_size]  # label
 
+    # 打乱顺序
+    p, h, label = shuffle(p, h, label)
 
+    # 序列中词转为索引，并将序列转化为等长的索引序列
+    p_c_index, h_c_index = char_index(p, h)
+
+    return
 
 
 
 if __name__ == '__main__':
-    load_all_data('../input/train.csv', data_size=100)
+    load_all_data(args.train_path, data_size=100)
